0e1dfbbab2ce79bed006c5c8243d0704fd6f3208
pulatov03 <pulatovubaydulla@gmail.com>
2021-04-09 10:51:14 +0500

    User
    added menu of user, and new directory

--- hospital management/menu/main_menu.h
+++ hospital management/menu/main_menu.h
@@ -1,53 +1,9 @@
 #include <string>
 #include <iostream>
+#include "user/user.h"
 //#include "../classes/accounts.h"
 using namespace std;
 
-void user_menu() {
-    int number;
-menu: 
-    system("cls");
-    cout << "Welcome to Hospital management system!" << endl;
-    cout << "Menu (Choose any option which you want) : " << endl;
-    cout << "1 Book doctor" << endl;
-    cout << "2 View active bookings" << endl;
-    cout << "3 Cencel booking" << endl;
-    cout << "4 About me" << endl;
-    cout << "5 Exit" << endl;
-    cout << "Enter the command: "; cin >> number;
-    switch (number)
-    {
-    case 1:
-        // book_doctor();
-        system("pause");
-        goto menu;
-        break;
-    case 2:
-        // view_active_bookings();
-        system("pause");
-        goto menu;
-        break;
-    case 3:
-        // cencel_booking();
-        system("pause");
-        goto menu;
-        break;
-    case 4:
-        // abou_me();
-        system("pause");
-        goto menu;
-        break;
-    case 5:
-        cout << "Good bye, thanks for using our system)" << endl;
-        break;
-    default:
-        cout << "You enter the wrong number, please retry" << endl;
-        system("pause");
-        goto menu;
-        break;
-    };
-
-}
 
 void main_menu(){
     int type = 0;
