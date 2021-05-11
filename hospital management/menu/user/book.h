80a3d3f2969acb26b861e1beb2178c9738fbecd8
pulatov03 <pulatovubaydulla@gmail.com>
2021-05-11 11:41:57 +0500

    last commit

--- hospital management/menu/user/book.h
+++ hospital management/menu/user/book.h
@@ -7,12 +7,12 @@ using namespace std;
 
 void book_doctor(User& user) {
     char* err;
-    int date, lastOrder{}, rc, maxId{};
+    int date, lastOrder{}, rc;
     // status check variable
-    string sql;
+    string sql, id;
     sqlite3* db;
     sqlite3_stmt* stmt; // result from execution
-    sqlite3_open("../hm_db.db", &db);
+    rc = sqlite3_open("../hm_db.db", &db);
     //prepare the sql 
 redate:
     cout << "Enter the date of booking (mm/dd/yyyy): "; cin >> date;
@@ -22,27 +22,15 @@ redate:
         if (lastOrder <= sqlite3_column_int(stmt, 0)) lastOrder = sqlite3_column_int(stmt, 0);
     }
     if (lastOrder >= 4) {
-        cout << "This day is busy, choose another day." << endl;
-        lastOrder = 0;
+        cout << "This day is busy, choose another day.";
         goto redate;
     }
     lastOrder = lastOrder + 1;
-    sql = "SELECT booking_id FROM Management";
-    sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, 0); //placing the result to stmt // preapre the table version of the resulta
-    while (sqlite3_step(stmt) != SQLITE_DONE) {
-        if (maxId < sqlite3_column_int(stmt, 0)) maxId = sqlite3_column_int(stmt, 0);
-    }
-    sqlite3_finalize(stmt);
-    sqlite3_close(db);
-
-    maxId = maxId + 1;
-    cout << maxId << endl << lastOrder << endl << date << endl;
-   // sqlite3_open("../hm_db.db", &db);
-    sql = "INSERT INTO Management VALUES (1,4," + to_string(user.getId()) + "," + to_string(date) + "," + to_string(lastOrder) + "," + to_string(maxId) + ")";
+    sql = "INSERT INTO Management VALUES (1,4," + to_string(user.getId()) + "," + to_string(date) + "," + to_string(lastOrder) + ")";
     rc = sqlite3_exec(db, sql.c_str(), NULL, NULL, &err);
     if (rc != SQLITE_OK) {
         cout << "Error: " << err;
         system("pause");
+        
     }
 };
-//Olimjon's code ends
